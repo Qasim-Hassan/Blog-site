0cd364a2bda7af55f51825567ef43efa44268155
thegulab123 <bygulab123@gmail.com>
2025-12-04 15:50:12 +0500

    fourth in the way

--- Ver-1.0/frontend/writescript.js
+++ Ver-1.0/frontend/writescript.js
@@ -1,7 +1,45 @@
 async function initApp(){
     const publish = document.getElementById("publish-btn");
-    
+    const title = document.getElementById("story-title");
+    const text = document.getElementById("story-textarea");
+
     publish.classList.add("disabled");
+
+    title.addEventListener("input",()=>{
+        if (text.value.trim()!=="" && title.value.trim()!==""){
+            publish.classList.remove("disabled");
+        }else{
+            publish.classList.add("disabled")
+        }
+
+    });
+
+    text.addEventListener("input",()=>{
+        if (text.value.trim()!=="" && title.value.trim()!==""){
+            publish.classList.remove("disabled");
+        }else{
+            publish.classList.add("disabled")
+        }
+
+    });
+
+    publish.addEventListener("click",()=>{
+        if (text.value.trim() ==="" || title.value.trim()===""){
+            return;
+        }
+
+        const data = {
+            title: title.value.trim(),
+            text: text.value.trim()
+        }
+
+        title.value = "",
+        text.value = ""
+
+        window.location.href = "index.html";
+    });
+
+    
 
     window.app = {}
 }
